0048ed70a4548404bc15e781fa36b9e01fb7b168
webifylab <webify.lab@inbox.ru>
2023-01-07 22:20:46 +0600

    update

--- Webify_Tele_Bot.py
+++ Webify_Tele_Bot.py
@@ -1,13 +1,27 @@
 import telebot
+from telebot import types
 
-bot = telebot.TeleBot('5610222750:AAEWIgLAxyFGJFLy4eGDkJq0bvrAE_YA4WA')
+bot = telebot.TeleBot('5861935603:AAHIJlSDAEzcT0f9CVd_Onapzkb4EyCLdxM')
 
 @bot.message_handler(commands=['start'])
-def start(mes):
-    bot.send_message(
-        mes.chat.id,    
-        '<b>Привет!</b>' , 
-        parse_mode="html"
-    )
 
-bot.polling(none_stop=True)
+def start(mes):
+    bot.send_message(mes.chat.id, "Привет! \n функции: \n /start - начать \n /send_application - отправить заявку")
+    
+@bot.message_handler(commands=['send_application'])
+
+def send_application(mes):  
+    global application;
+    while application == '': #проверяем что возраст изменился
+        try:
+            application = int(mes.text) #проверяем, что возраст введен корректно
+        except Exception:
+            bot.send_message(mes.from_user.id, 'Цифрами, пожалуйста');
+            keyboard = types.InlineKeyboardMarkup(); #наша клавиатура
+            key_yes = types.InlineKeyboardButton(text='Да', callback_data='yes'); #кнопка «Да»
+            keyboard.add(key_yes); #добавляем кнопку в клавиатуру
+            key_no= types.InlineKeyboardButton(text='Нет', callback_data='no');
+            keyboard.add(key_no);
+            bot.send_message(mes.from_user.id, "", reply_markup=keyboard)
+
+bot.polling()
